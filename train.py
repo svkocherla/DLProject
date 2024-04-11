e50c4fa0cb8db8f666bc8427b8c5eb2497365e1f
Henry Liao <hliao62@gatech.edu>
2024-04-11 11:27:12 -0400

    Add training loop for PPO

--- train.py
+++ train.py
@@ -84,7 +84,7 @@ def train_reinforce(model, reward, optimizer="adam", learning_rate=.01):
     return model
 
 
-def train_ppo(model, reward, optimizer="adam", learning_rate=.01):
+def train_ppo(model, reward, optimizer="adam", learning_rate=.01, gamma = .9, epsilon =.2, clip = .1):
     out_actions = 4
 
     game = Grid()
@@ -101,19 +101,33 @@ def train_ppo(model, reward, optimizer="adam", learning_rate=.01):
         states = []
         action_buffer = []
         reward_buffer = []
+        value_buffer = []
 
         while not game.is_solved():
             state = game.get_state()
-            action_dist = torch.softmax(model(state)) # depends on if model output is logits or values
+            logits = model(state)
+            action_dist = torch.softmax(logits) # depends on if model output is logits or values
             action = np.random.choice(actions, p = action_dist.numpy())
             action_buffer.append(action)
         
             r = reward(state, action)
             reward_buffer.append(reward)
 
+            value = torch.max(logits)
+
+            value_buffer.append(value)
+
             game.process_move(action)
 
         reward_buffer = torch.tensor(reward_buffer)
+        value_buffer = torch.tensor(value_buffer)
+        returns = calculate_ppo_returns(returns, gamma)
+
+        adv = returns - value_buffer
+
+        adv = (adv - torch.mean(adv))/ torch.std(adv)
+
+        update_ppo(model, optimizer, states, action_buffer, returns, adv, epsilon, clip)
 
 
     for step in range(len(states)):
@@ -129,6 +143,51 @@ def train_ppo(model, reward, optimizer="adam", learning_rate=.01):
         opt.step()
     
     return model
+
+def calculate_ppo_returns(reward_buffer, gamma):
+    returns = []
+    R = 0
+    for r in range(len(reward_buffer)):
+        reward = reward_buffer[len(reward_buffer)-r-1]
+        R = reward + gamma * R
+        returns.append(R)
+
+    returns = reversed(returns)
+    return torch.tensor(returns)
+
+def update_ppo(model, optimizer, states, actions_buffer, returns, advantages, epsilon, clip_threshold):
+    model.eval()
+    
+    logits = torch.tensor()
+    
+    for observation in states:
+        output = model(state)
+        logits = torch.vstack(logits, output)
+
+    probs = torch.softmax(logits, dim=-1)
+    action_probs = probs.gather(1, actions_buffer.unsqueeze(1))
+        
+    old_logits = logits.detach()
+    old_probs = torch.softmax(old_logits, dim=-1).detach()
+    old_action_probs = old_probs.gather(1, action_buffer.unsqueeze(1))
+
+    ratio = action_probs / old_action_probs
+    surrogate1 = ratio * advantages
+    surrogate2 = torch.clamp(ratio, 1.0 - epsilon, 1.0 + epsilon) * advantages
+    actor_loss = -torch.mean(torch.min(surrogate1, surrogate2))
+
+
+
+    thresh = old_action_probs + torch.clamp(probs - old_probs, -clip_threshold, clip_threshold)
+    value_loss = torch.mean(torch.max((returns - clipped_values)**2, (returns - old_action_probs)**2))
+
+    total_loss = actor_loss + value_loss
+    optimizer.zero_grad()
+    total_loss.backward()
+    optimizer.step()
+
+
+
 
 
 
