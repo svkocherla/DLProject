69aa2e2fa7a25e3ac66edadc00c298fcece042bb
sriharshak27 <sriharshakocherla27@gmail.com>
2024-04-17 17:20:24 -0400

    best 3x3 so far

--- train.py
+++ train.py
@@ -6,15 +6,15 @@ from util.enums import *
 
 if __name__ == "__main__":
     grid_size = 3
-    learning_rate = 0.005
+    learning_rate = 0.001
     discount_factor = 0.95
     epsilon = 0.1
-    max_episodes = 10000
+    max_episodes = 20000
 
     # turn off verbose if you want
     env = Grid(grid_size)
     dqn = QNetwork(grid_size, learning_rate, discount_factor, epsilon)
     learning = QLearning(dqn, max_episodes)
-    learning.train(env, verbose = True, shuffle_cap=10) # use shuffle cap next time
+    learning.train(env, verbose = True, shuffle_cap=20) # use shuffle cap next time
     dqn.save_model("q_learning/models/Qnet3x3")
     learning.run_tests(env, num_tests=1000, verbose=False)
