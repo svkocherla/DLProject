bb3da60c22b54e3f719039c7994ce47742dfca57
sriharshak27 <sriharshakocherla27@gmail.com>
2024-04-17 20:26:14 -0400

    best 4x4 model so far, look at hyperparameters

--- train.py
+++ train.py
@@ -5,16 +5,16 @@ from simulator.game15 import *
 from util.enums import *
 
 if __name__ == "__main__":
-    grid_size = 5
-    learning_rate = 0.001
+    grid_size = 4
+    learning_rate = 0.0005
     discount_factor = 0.95
     epsilon = 0.1
-    max_episodes = 10000
+    max_episodes = 20000
 
     # turn off verbose if you want
     env = Grid(grid_size)
     dqn = QNetwork(grid_size, learning_rate, discount_factor, epsilon)
     learning = QLearning(dqn, max_episodes)
     learning.train(env, verbose = True, shuffle_cap=50) # use shuffle cap next time
-    dqn.save_model("q_learning/models/Qnet5x5")
+    dqn.save_model("q_learning/models/Qnet4x4")
     learning.run_tests(env, num_tests=1000, verbose=False)
