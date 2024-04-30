da9d2f2934e621fcf16ca0161d928257ae9ad51d
sriharshak27 <sriharshakocherla27@gmail.com>
2024-04-29 20:57:41 -0400

    tests on 4x4 500000 episodes

--- vis.py
+++ vis.py
@@ -17,13 +17,13 @@ def plot_accs(shuffle, res):
     q_test = QNetwork(grid_size, learning_rate, discount_factor, epsilon)
     q_learning = QLearning(q_test, max_episodes)
     
-    dir = 'q_learning/models/Qnet4x4R'
+    dir = 'q_learning/models/Qnet4x4mil'
     
     checkpoints = []
     accuracies = []
     
     env = Grid(4)
-    for i in range(0,100000, 1000):
+    for i in range(0, 500000, 3000):
         try:
             q_test.load_model(f"{dir}/{i}")
             accuracy = q_learning.run_tests(env, num_tests=1000, verbose=False, max_shuffle=shuffle, step_limit=100, set_shuffle=True)
@@ -48,4 +48,49 @@ def plot_accs(shuffle, res):
     plt.savefig(f"{res}.png")
     plt.close()
 
-plot_accs(10, 'graphs/max10')
+# plot_accs(15, 'graphs/mil15')
+# plot_accs(20, 'graphs/mil20')
+
+
+
+def plot_acc_vs_shuffle(res):
+    grid_size = 4
+    learning_rate = 0.00015
+    discount_factor = 0.95
+    epsilon = 0.1
+    max_episodes = 100000
+
+    # turn of verbose if you want
+    env = Grid(grid_size)
+    q_test = QNetwork(grid_size, learning_rate, discount_factor, epsilon)
+    q_learning = QLearning(q_test, max_episodes)
+    
+    dir = 'q_learning/models/Qnet4x4mil'
+    q_test.load_model(f"{dir}/497000")
+    
+    checkpoints = []
+    accuracies = []
+    
+    env = Grid(4)
+    for i in range(1,51):
+        accuracy = q_learning.run_tests(env, num_tests=1000, verbose=False, max_shuffle=i, step_limit=100, set_shuffle=True)
+        checkpoints.append(i)
+        accuracies.append(accuracy)
+        print(f"Shuffle: {i}, Acc: {accuracy}")
+
+    tups = sorted(zip(checkpoints, accuracies))
+    checkpoints, accuracies = zip(*tups)
+
+    
+    # plot graph
+    plt.figure(figsize=(10, 6))
+    plt.plot(checkpoints, accuracies, marker='o')
+    plt.xlabel("Shuffle Distance")
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy vs Shuffle Distance")
+    plt.grid(True)
+    # plt.show()
+    plt.savefig(f"{res}.png")
+    plt.close()
+
+plot_acc_vs_shuffle('graphs/AccVsShuffle2')
